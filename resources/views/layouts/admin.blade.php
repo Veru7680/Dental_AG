8db0d14838bd4d5d21465d12e92afe1e368de253
Veru7680 <veru6198@gmail.com>
2025-09-29 18:31:17 -0400

    agregar Font Awesome

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,9 +11,12 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+        <!-- fontawesome -->
+        <script src="https://kit.fontawesome.com/29c8011380.js" crossorigin="anonymous"></script>
+       
         <!-- Styles -->
         @livewireStyles
     </head>

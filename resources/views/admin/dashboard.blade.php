26f1481ece80d7c0486e51e6418e06a9d5d6a597
Veru7680 <veru6198@gmail.com>
2025-09-30 11:51:20 -0400

    agregar título de página y breadcrumbs dinámicos

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,19 @@
-<x-admin-layout>
+<x-admin-layout 
+title="Clinica AG" {{-- Aquí cambia el título de la página --}}
+:breadcrumbs="[
+    [
+        'name'=>'Dashboard',
+        'href'=> route('admin.dashboard'),
+    ],
+
+    [
+        'name'=>'prueba',
+    ]
+    
+    ]">
+    <x-slot name="action">
+       prosicion de boton
+    </x-slot>
 
 hola desed
 </x-admin-layout>
